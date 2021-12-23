b86324c85e647b5616e6b1cd4c132a9785311a17
nhsb00 <nhsb00@hotmail.com>
2021-12-23 08:30:58 -0500

    reviewing python

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
+//sample 1
 for (let i = 0; i < 10; i++) {
     console.log(i, Math.pow(2, i))
 }
-
+//sample 2
 function factorial(num) {
     if (num <= 1) {
         return 1
@@ -12,3 +13,22 @@ function factorial(num) {
 
 result = factorial(4)
 console.log(result)
+
+//sample 3
+
+function validate(num) {
+    if (num < 1 || num > 9) {
+        console.log('Out of range')
+    } else if (num != parseInt(num)) {
+        console.log('Not an integer')
+    } else {
+        console.log('Just right')
+        return true
+    }
+    return false
+}
+
+console.log(validate(-5))
+console.log(validate(15))
+console.log(validate(-5.2))
+console.log(validate(2))
--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 #sample # 1
 for i in range(0,10):
     print (i, 2**i)
-
+#sample # 2
 def factorial(num):
     if num <= 1:
         return 1
@@ -9,3 +9,18 @@ def factorial(num):
         return num * factorial(num-1)
 result = factorial(4)
 print(result)
+
+#sample 3
+def validate(num):
+    if (num < 1 or num > 9):
+        print('Out of range')
+    elif (num != int(num)):
+        print('Not an integer')
+    else:
+        return True
+    return False
+
+print (validate(-5))
+print (validate(15))
+print (validate(-5.2))
+print (validate(2))
