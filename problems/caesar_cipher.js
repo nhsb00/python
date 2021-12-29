3189ad84604826e544fe490d57ee33516877dbbd
nhsb00 <nhsb00@hotmail.com>
2021-12-29 12:53:43 -0500

    caesar cipher

--- problems/caesar_cipher.js
+++ problems/caesar_cipher.js
@@ -12,3 +12,20 @@
 // Examples: 
 // caesarCipher(“abc”, 2) => “cde”
 // caesarCipher(“xyz”, 1) => “yza"
+
+function caesarCipher(str, shift) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let encoded = "";
+
+    for (let i = 0; i <str.length; i++) {
+        if (str[i] === " ") {
+            encoded += " ";
+            continue
+        }
+
+        const offset = (alphabet.indexOf(str[i] + shift) % 26);
+        encoded += alphabet[offset];
+    }
+    return encoded;
+
+}
