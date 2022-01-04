142c0ab6512334359f9ee20dadaedd378f337023
nhsb00 <nhsb00@hotmail.com>
2022-01-04 13:45:22 -0500

    variables

--- PythonSpartacoding/hello.py
+++ PythonSpartacoding/hello.py
@@ -1,1 +1,12 @@
-print('hello sparta')
+# print('hello sparta')
+
+# Variables
+# a=3
+# b=2
+# print(a+b)
+# print(a ** b) #square 제곱
+# print(a % b) #mod 나머지
+
+# a = 'soobin' #can be number and string and boolean
+# a = (3 > 2)
+# print(a)
