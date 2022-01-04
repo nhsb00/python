9bd3cbf4cdd5f00093c6db638109629cb3b0d682
nhsb00 <nhsb00@hotmail.com>
2022-01-04 18:04:23 -0500

    dictionary

---
+++ PythonSpartacoding/dictionary.py
@@ -0,0 +1,13 @@
+# list 순서가 중요
+# dictionary key and value
+
+a_list = ['apple', 'pear', 'orange', 2, True, ['a','b']]
+a_list.append(99) #add 추가
+
+print(a_list)
+# people = [
+#     {'name': 'bob', 'age': 20, 'score':{'math':90,'science':70}},
+#     {'name': 'carry', 'age': 38, 'score':{'math':40,'science':72}},
+#     {'name': 'smith', 'age': 28, 'score':{'math':80,'science':90}},
+#     {'name': 'john', 'age': 34, 'score':{'math':75,'science':100}}
+# ]
--- PythonSpartacoding/hello.py
+++ PythonSpartacoding/hello.py
@@ -10,3 +10,35 @@
 # a = 'soobin' #can be number and string and boolean
 # a = (3 > 2)
 # print(a)
+
+#String
+
+# first_name = 'soobin'
+# t_name = "lim"
+
+# print (first_name + last_name)
+
+# a = 2 # a = 2 and a = '2' are different. They can't add them up. a = '2' and a = str(2) are same
+# b = a
+
+# print(b)
+
+# text = "abcdefghijk"
+
+# result = len(text)
+
+# result = text[:3] # 0,1,2
+# result = text[3:] # 4~~~end
+# result = text[3:8] # 4,5,6,7,8
+
+# print(result)
+
+# myemail = 'abc@google.com'
+
+# result = myemail.split('@')[1].split('.')[0]
+# print(result)
+
+phone = '347-968-7263'
+result = phone.split('-')[0]
+
+print(result)
