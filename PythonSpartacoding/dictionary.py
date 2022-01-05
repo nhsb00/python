c9863a1a8b5f5172b60fd1187d486d0f4cec285f
nhsb00 <nhsb00@hotmail.com>
2022-01-05 10:44:32 -0500

    dictionary

--- PythonSpartacoding/dictionary.py
+++ PythonSpartacoding/dictionary.py
@@ -1,10 +1,22 @@
 # list 순서가 중요
 # dictionary key and value
 
-a_list = ['apple', 'pear', 'orange', 2, True, ['a','b']]
-a_list.append(99) #add 추가
+# a_list = ['apple', 'pear', 'orange', 2, True, ['a','b']]
+# a_list.append(99) #add 추가
+# a_list.sort()
+# a_list.sort(reverse=True)
+# result = (5 in a_list)
 
-print(a_list)
+# print(a_list)
+# print(result)
+
+#Dictionary
+# a_dict = { 'name' : 'bob', 'age' : 27, 'friend' : ['aidan', 'eric']}
+# result = a_dict['friend']
+
+# print(result)
+# print('height' in a_dict)
+
 # people = [
 #     {'name': 'bob', 'age': 20, 'score':{'math':90,'science':70}},
 #     {'name': 'carry', 'age': 38, 'score':{'math':40,'science':72}},
