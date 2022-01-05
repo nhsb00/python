33419c677a0377f2d910cba02ba89c7b5a3bff01
nhsb00 <nhsb00@hotmail.com>
2022-01-05 11:05:13 -0500

    loop

---
+++ PythonSpartacoding/if.py
@@ -0,0 +1,59 @@
+#if elif else
+# money = 6000
+
+# if money > 3800:
+#     print('catch the taxi')
+# elif money > 1200:
+#     print ('ride on bus')
+# else:
+#     print('take a walk')
+
+# loop
+
+# fruits = ['apple', 'pear', 'watermelon', 'strawberry', 'kiwi']
+
+# for fruit in fruits:
+#     print(fruit)
+
+people = [
+    {'name': 'bob', 'age': 20},
+    {'name': 'carry', 'age': 38},
+    {'name': 'john', 'age': 7},
+    {'name': 'smith', 'age': 17},
+    {'name': 'ben', 'age': 27},
+    {'name': 'bobby', 'age': 57},
+    {'name': 'red', 'age': 32},
+    {'name': 'queen', 'age': 25}
+]
+
+# for person in people:
+#     name = person['name']
+#     age = person['age']
+#     if age > 20:
+#         print(name, age)
+
+# for i, person in enumerate(people):
+#     name = person['name']
+#     age = person['age']
+#     print(i, name, age)
+#     if i > 2:
+#         break
+
+num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
+
+# count = 0
+# for num in num_list:
+#     if num % 2 == 0:
+#        count += 1
+
+# print(count)
+# sum = 0
+# for num in num_list:
+#     sum += num
+# print(sum)
+
+# big = 0
+# for num in num_list:
+#     if num > big:
+#         big = num
+# print(big)
